93b8da5225569675b83d77b34e3980d86f5da5ee
ledragon02 <165173435+ledragon02@users.noreply.github.com>
2025-03-06 16:52:25 +0100

    Update script.bat

--- PID-Windows/script.bat
+++ PID-Windows/script.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: 1️⃣ Détecter la lettre de la clé USB
+::  Détecter la lettre de la clé USB
 for /f "tokens=1,2 delims= " %%A in ('wmic logicaldisk where "DriveType=2" get DeviceID^, VolumeName ^| find ":"') do (
     set USB_DRIVE=%%A
 )
 
-:: 2️⃣ Vérifier si une clé USB a été détectée
+::  Vérifier si une clé USB a été détectée
 if "%USB_DRIVE%"=="" (
-    echo ❌ Aucune clé USB détectée.
+    echo  Aucune clé USB détectée.
     exit /b
 )
 
@@ -20,4 +20,3 @@ clamscan -r --remove "%USB_DRIVE%\"
 
 echo Scan terminé.
 pause
-
