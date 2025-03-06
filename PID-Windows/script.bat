6eba11f29f4971e55b2527ba483ff48b38d4b586
YARA <charlesyara5@gmail.com>
2025-03-06 16:48:25 +0100

    j’ai fait un script pour scanner une clé USB quand elle est insérée sur le pc

---
+++ PID-Windows/script.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: 1️⃣ Détecter la lettre de la clé USB
+for /f "tokens=1,2 delims= " %%A in ('wmic logicaldisk where "DriveType=2" get DeviceID^, VolumeName ^| find ":"') do (
+    set USB_DRIVE=%%A
+)
+
+:: 2️⃣ Vérifier si une clé USB a été détectée
+if "%USB_DRIVE%"=="" (
+    echo ❌ Aucune clé USB détectée.
+    exit /b
+)
+
+echo Clé USB détectée : %USB_DRIVE%\
+
+
+echo Scan en cours...
+clamscan -r --remove "%USB_DRIVE%\"
+
+echo Scan terminé.
+pause
+
