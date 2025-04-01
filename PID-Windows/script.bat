39d91912c5c681dcfd640d213806d4d69666fbe3
YARA <charlesyara5@gmail.com>
2025-04-01 10:46:06 +0200

    Update script.bat, pour lancé le  scan clamav dans une autre cmd

--- PID-Windows/script.bat
+++ PID-Windows/script.bat
@@ -14,9 +14,11 @@ if "%USB_DRIVE%"=="" (
 
 echo Clé USB détectée : %USB_DRIVE%\
 
-
+:: Créer un fichier temporaire pour les logs
+set LOGFILE=%TEMP%\clamscan_log.txt
+​
+echo Les logs sont enregistrés dans : %LOGFILE%
 echo Scan en cours...
-clamscan -r --remove "%USB_DRIVE%\"
-
-echo Scan terminé.
+start /min cmd /c "clamscan -r --remove "%USB_DRIVE%\" > "%LOGFILE%" 2>&1"
+​
 pause
